8c3d8b08d4564af8ef33c4ea0fbbc0ac83410bf2
Rohit Selote <workforrohitselote@gmail.com>
2026-01-28 22:49:35 +0530

    Update L2A1.py

--- Problem_py/L2A1.py
+++ Problem_py/L2A1.py
@@ -4,3 +4,13 @@
 # If current < 0.5 A, print “Low current” 
 # If 0.5 A ≤ current ≤ 2 A, print “Normal current” 
 # If current > 2 A, print “High current 
+v=float(input("Enter voltage"))
+r=float(input("Enter resistance"))
+i=float(v/r)
+print(i)
+if i<0.5:
+    print("low current")
+elif 0.5<=i<=2 :
+    print("Normal current")
+else:
+    print("high currrent")
