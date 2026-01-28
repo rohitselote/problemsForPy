3a02cce191ec90edf388ec6fc4010fab10d306e9
Rohit Selote <workforrohitselote@gmail.com>
2026-01-28 22:42:55 +0530

    Create L1A1.py

---
+++ Problem_py/L1A1.py
@@ -0,0 +1,6 @@
+# a) Develop a Python program that takes a voltage (V) and resistance (R) as inputs from the user and calculates the 
+# current (I) using Ohm’s Law. 
+# b) Modify the above program to display the nature of current: 
+# If current < 0.5 A, print “Low current” 
+# If 0.5 A ≤ current ≤ 2 A, print “Normal current” 
+# If current > 2 A, print “High current 
